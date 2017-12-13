83d750317b0488215d2bf9ba79f005b7d4d3cc9b
Thanos <apk337@nyu.edu>
2017-12-12 20:22:03 -0500

    more and more changes

--- fig6/fig6-set-tc.sh
+++ fig6/fig6-set-tc.sh
@@ -16,14 +16,14 @@ sudo ethtool -K eth"$1" gso off tso off gro off;
 tc qdisc del dev eth"$1" root
 tc qdisc add dev eth"$1" root handle 1: htb default 10
 tc class add dev eth"$1" parent 1: classid 1:10 htb rate 100Mbit
-tc qdisc add dev eth"$1" parent 1:10 handle 20: netem delay 5ms limit 1024 
+tc qdisc add dev eth"$1" parent 1:10 handle 20: netem delay 5ms limit 1024
 
 
 ## For server - ifb0 
 tc qdisc del dev ifb0 root
 tc qdisc add dev ifb0 root handle 1: htb default 10
 tc class add dev ifb0 parent 1: classid 1:10 htb rate 100Mbit
-tc qdisc add dev ifb0 parent 1:10 handle 20: netem delay 5ms limit 1024 
+tc qdisc add dev ifb0 parent 1:10 handle 20: netem delay 5ms limit 1024
 
 sudo ethtool -K eth"$1" gso off tso off gro off
 sudo ethtool -K ifb0 gso off tso off gro off
